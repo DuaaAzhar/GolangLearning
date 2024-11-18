781569343a98fce39f9faf2b76c8db741d0adc46
DuaaAzharBIM <duaa.azhar@bigimmersive.com>
2024-11-18 18:23:20 +0500

    section5Complete_ Structs&CustomTypes

--- 5_section_Structs&CustomTypes/07_PracticeProject/main.go
+++ 5_section_Structs&CustomTypes/07_PracticeProject/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/practice/note"
 )
@@ -15,17 +18,25 @@ func main() {
 	}
 
 	myNote := note.New(title, content)
+	myNote.Display()
+	errSave := myNote.Save()
+
+	if errSave != nil {
+		fmt.Println("Saving to File Failed!!")
+	} else {
+		fmt.Println("Saving to File Succeeded!!")
+	}
 
 }
 
 func getNoteData() (string, string, error) {
-	title, err := getUserInput("Note Title: ")
+	title, err := getLongUserInput("Note Title: ")
 
 	if err != nil {
 		return "", "", err
 	}
 
-	content, err := getUserInput("Note Content: ")
+	content, err := getLongUserInput("Note Content: ")
 	if err != nil {
 		return "", "", err
 	}
@@ -36,8 +47,24 @@ func getUserInput(prompt string) (string, error) {
 	fmt.Println(prompt)
 	var value string
 	fmt.Scan(&value)
-	if value != "" {
+	if value == "" {
 		return "", errors.New("invalid input")
 	}
 	return value, nil
+}
+
+func getLongUserInput(prompt string) (string, error) {
+	fmt.Printf("%v", prompt)
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text, nil
+
 }
